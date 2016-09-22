0fad42c0f2929665b8db9ed5ede2c2b583d6a0fe
László Ilonka <laszlo.gogolak@gmail.com>
2016-09-22 15:10:23 +0200

    Update initializer scripts

--- initialize-wily.sh
+++ initialize-wily.sh
@@ -8,10 +8,12 @@ echo y | apt-get install git
 echo y | apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 touch /etc/apt/sources.list.d/docker.list
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | tee --append /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo ubuntu-wily main' | tee --append /etc/apt/sources.list.d/docker.list
 apt-get update
 echo y | apt-get install docker-engine
 gpasswd -a jenkins docker
+rm -R /var/lib/jenkins/*
+cp -R ./* /var/lib/jenkins/
+chown -R jenkins:jenkins /var/lib/jenkins/*
 service docker restart
 service jenkins restart
-
